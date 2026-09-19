597236493d948cd0d177664cf7a83c26a10deee5
PanaitAlessandro <14529922@itisrossi.vi.it>
2026-09-19 14:48:10 +0200

    aggiunto CCerchio IS-A e gestito il base()

--- Ereditarieta/CCerchio.cs
+++ Ereditarieta/CCerchio.cs
@@ -2,5 +2,41 @@ using System;
 
 namespace CerchioEreditarieta
 {
+
+    public class CCerchio : CPunto // uso i due punti perch è è la convenzione per la ISA
+    {
+        private double _raggio;
+
+        // dato che siamo sotto forma ISA (eredità); non devo mettere un campo 'private CPunto centro;'
+
+        public double Raggio
+        {
+            get {return _raggio;}
+            set
+            {
+                if(value <=0)
+                {
+                    throw new ArgumentException("il raggio non può essere minore di zero!");
+                }
+                _raggio = value;
+            }
+        }
+
+        public CCerchio()
+        {
+            Raggio = 1;
+        }
+
+        public CCerchio(double raggio, int x,int y) : base(x, y) // base() in questo caso richiama il costruttore di CPunto e gli da i parametri
+        {
+            Raggio = raggio;
+        }
+
+        public override string ToString()
+        {
+            return $"Raggio: {Raggio}, Centro: {base.ToString()}";
+        }
+
+    }
     
 }
