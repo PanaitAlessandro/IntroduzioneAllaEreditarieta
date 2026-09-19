a4e9ac100bb20dda1b2917d99a2284744a263660
PanaitAlessandro <14529922@itisrossi.vi.it>
2026-09-19 18:49:37 +0200

    feat(Ereditarieta): aggiunta la classe CCilindro IS-A completa

--- Ereditarieta/CCilindro.cs
+++ Ereditarieta/CCilindro.cs
@@ -2,5 +2,36 @@ using System;
 
 namespace CerchioEreditarieta
 {
-    
+    public class CCilindro : CCerchio
+    {
+        private double _altezza;
+
+        public double Altezza
+        {
+            get { return _altezza; }
+            set
+            {
+                if(value <= 0)
+                {
+                    throw new ArgumentException("HEY! L'altezza non può avere valori <= 0");
+                }
+                _altezza = value;
+            }
+        }
+
+        public CCilindro()
+        {
+            Altezza = 1;
+        }
+
+        public CCilindro(double altezza, double raggio, int x, int y) : base(raggio, x, y)
+        {
+            Altezza = altezza;
+        }
+
+        public override string ToString()
+        {
+            return $"Altezza: {Altezza}, Base: {base.ToString()}";
+        } 
+    }
 }
