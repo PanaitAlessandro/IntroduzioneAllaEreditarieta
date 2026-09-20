dc0257b6cf6054df365c5d1bdd56978c682f6054
PanaitAlessandro <14529922@itisrossi.vi.it>
2026-09-20 15:01:41 +0200

    feat(composizione): aggiunto Program.cs + gitignore

--- Composizione/Program.cs
+++ Composizione/Program.cs
@@ -1,6 +1,13 @@
-using System;
+﻿using System;
 
 namespace CerchioComposizione
 {
-    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CCilindro cilindro = new CCilindro(5.0, 2.0,1,2); // devo mettere double altezza, double raggio, int x, int y
+            System.Console.WriteLine(cilindro);
+        }
+    }
 }
